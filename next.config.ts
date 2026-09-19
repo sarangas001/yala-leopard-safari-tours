42937e473785ca1d489dfebfaf4a113e5d20123f
saranaga <sarangasama@gmail.com>
2026-09-19 17:03:59 +0530

    feat: update image sizes and quality settings for improved performance and visual consistency

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    qualities: [55, 70, 72, 75, 80, 90],
+    qualities: [55, 70, 72, 75, 80, 85, 90],
   },
 };
 
